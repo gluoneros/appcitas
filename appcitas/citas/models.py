43d8a6c554773bc568bdbb7a9b9fb6ea44e3157f
gluoneros <gluoneros@gmail.com>
2023-10-04 22:28:17 -0500

    4/10/23

--- appcitas/citas/models.py
+++ appcitas/citas/models.py
@@ -1,3 +1,26 @@
+"""Citas models."""
+
+#Django
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """User model"""
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    is_admin = models.BooleanField(default=False)
+    
+    bio = models.TimeField(blank=True, null=True)
+    
+    birthdate = models.DateField(blank=True, null=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        """Return email."""
+        return self.email
